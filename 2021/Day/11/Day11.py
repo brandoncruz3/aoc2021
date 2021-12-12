d0f6b938e3500bb74d5afb93cc33548b2b5a7dca
Brandon Cruz <brandon@adhocteam.us>
2021-12-12 13:16:58 -0500

    Day 12 catchup

---
+++ 2021/Day/11/Day11.py
@@ -0,0 +1,32 @@
+from aocd import data
+from zgrid import ZGrid
+
+g = ZGrid(data, transform=int)
+a = b = 0
+while True:
+    b += 1
+    n = 0
+    flash = []
+    for z in g:
+        if g[z] == 9:
+            g[z] = 0
+            flash.append(z)
+        else:
+            g[z] += 1
+    while flash:
+        z0 = flash.pop()
+        n += 1
+        for z in g.near(z0, n=8):
+            if g.get(z):
+                if g[z] == 9:
+                    g[z] = 0
+                    flash.append(z)
+                else:
+                    g[z] += 1
+    if b <= 100:
+        a += n
+    if n == len(g):
+        break
+
+print("part a:", a)
+print("part b:", b)
